57ce5c89e858d979bab43e202de37e8b4dedc9fa
Marc Thiry <marc.thiry@edhec.com>
2017-11-24 16:25:54 +0100

    grid template home

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,4 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.3.2/js/mdb.min.js"></script>
